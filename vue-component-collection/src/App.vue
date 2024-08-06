98ef497ed323b3a557b502934f3019158a4c2149
Sif Høg <cimmik@yahoo.dk>
2024-08-06 19:13:04 +0200

    Created divs with flex
    Next, you should add the SVGs to scroll divisions.

--- vue-component-collection/src/App.vue
+++ vue-component-collection/src/App.vue
@@ -1,8 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import DateRangeInput from "./components/DateRangeInput.vue";
+</script>
 
 <template>
   <h1>Vue Component COllection</h1>
   <p>Written by <em>Sif Høg-Getgood</em></p>
+  <p>This is a collection of vue components I've created.</p>
+  <DateRangeInput :display-fineness="['months', 'days', 'years']" />
 </template>
 
 <style scoped></style>
---
+++ vue-component-collection/src/components/DateRangeInput.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+type DateRangeInputProps = {
+  displayFineness: Array<"days" | "months" | "years">;
+  width?: CSSUnit;
+};
+type _CSSUnitSuffix = `${"" | " "}${
+  | "cm"
+  | "mm"
+  | "in"
+  | "px"
+  | "pt"
+  | "pc"
+  | "em"
+  | "ex"
+  | "ch"
+  | "rem"
+  | "vw"
+  | "vh"
+  | "vmin"
+  | "vmax"
+  | "%"}`;
+type CSSUnit = `${number}${_CSSUnitSuffix}` | "0";
+const test = "100%";
+
+const props = withDefaults(defineProps<DateRangeInputProps>(), {
+  width: "100%",
+});
+</script>
+
+<template>
+  <div class="date-range-input" :style="`width:${props.width}`">
+    <div class="scroll-left"></div>
+    <div class="date-container">
+      <div
+        v-if="props.displayFineness.includes('days')"
+        class="day-container container"
+      ></div>
+      <div
+        v-if="props.displayFineness.includes('months')"
+        class="month-container container"
+      ></div>
+      <div
+        v-if="props.displayFineness.includes('years')"
+        class="year-container container"
+      ></div>
+    </div>
+    <div class="scroll-right"></div>
+  </div>
+</template>
+
+<style scoped>
+div {
+  border: solid black 1px;
+  min-height: 10px;
+  min-width: 10px;
+}
+.date-container {
+  flex-grow: 1;
+}
+.scroll-left {
+  flex-basis: 3rem;
+}
+.scroll-right {
+  flex-basis: 3rem;
+}
+.date-range-input {
+  display: flex;
+}
+</style>
