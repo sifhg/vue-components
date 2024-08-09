f3b87bae83f0f8ed1b3d13732f8bbe4c75be2bbc
Sif Høg <cimmik@yahoo.dk>
2024-08-09 12:06:24 +0200

    Adjusting width of DateRangeInput2

--- vue-component-collection/src/components/DateRangeInput2.vue
+++ vue-component-collection/src/components/DateRangeInput2.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
 import GoodCanvas from "./DateRangeInput2/GoodCanvas/CanvasAPI";
+import { Colour } from "./DateRangeInput2/GoodCanvas/Colour";
 
 type DateRangeInputProps = {
   displayFineness: Array<"days" | "months" | "years">;
@@ -9,16 +10,26 @@ type DateRangeInputProps = {
   lastDate?: Date;
 };
 const props = withDefaults(defineProps<DateRangeInputProps>(), {
-  width: 500,
   unitSize: 32,
   lastDate: () => {
     return new Date();
   },
 });
 
+const width = ref<number | undefined>(props.width);
+
 const canvas = ref<GoodCanvas>();
 onMounted(() => {
+  if (!width.value) {
+    width.value = document.getElementById(
+      "date-selection-canvas"
+    )?.parentElement?.clientWidth;
+  }
+
   canvas.value = new GoodCanvas("date-selection-canvas");
+  canvas.value.setCanvasSize(width.value!, props.unitSize * 5);
+  canvas.value.background(new Colour("GREYSCALE", 207));
+  canvas.value.render(true);
 });
 </script>
 
